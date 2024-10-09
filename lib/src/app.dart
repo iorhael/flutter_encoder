a97f2081ea903eac6ed68cc05a80105fb4084f2f
Mikhail Skliar <132287268+iorhael@users.noreply.github.com>
2024-10-09 23:05:23 +0300

    Add file picking functionality (#1)
    * Add picker button
    
    * Add picker functionality

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -2,19 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
-import 'sample_feature/sample_item_details_view.dart';
-import 'sample_feature/sample_item_list_view.dart';
+import 'file_picker_feature/file_picker_view.dart';
+import 'file_picker_feature/file_picker_controller.dart';
 import 'settings/settings_controller.dart';
 import 'settings/settings_view.dart';
 
 /// The Widget that configures your application.
 class MyApp extends StatelessWidget {
-  const MyApp({
+  MyApp({
     super.key,
     required this.settingsController,
   });
 
   final SettingsController settingsController;
+  final FilePickerController filePickerController = FilePickerController();
 
   @override
   Widget build(BuildContext context) {
@@ -69,11 +70,8 @@ class MyApp extends StatelessWidget {
                 switch (routeSettings.name) {
                   case SettingsView.routeName:
                     return SettingsView(controller: settingsController);
-                  case SampleItemDetailsView.routeName:
-                    return const SampleItemDetailsView();
-                  case SampleItemListView.routeName:
                   default:
-                    return const SampleItemListView();
+                    return FilePickerView(controller: filePickerController);
                 }
               },
             );
--- lib/src/settings/settings_view.dart
+++ lib/src/settings/settings_view.dart
@@ -17,7 +17,7 @@ class SettingsView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Some shit'),
+        title: const Text('Choose theme'),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16),
