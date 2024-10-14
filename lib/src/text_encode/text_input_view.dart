a030936d51aa2a2f1f613397998e4937b122f19b
Mikhail Skliar <skliarmikhail@gmail.com>
2024-10-14 23:43:25 +0300

    Add history_view and text_input_view

---
+++ lib/src/text_encode/text_input_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TextInputView extends StatelessWidget {
+  const TextInputView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        'Text Input View',
+        style: TextStyle(fontSize: 24),
+      ),
+    );
+  }
+}
